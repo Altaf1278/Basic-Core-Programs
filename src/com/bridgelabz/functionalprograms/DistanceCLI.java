e7fbd43f642370287affdfb79e75d36ec36457fe
altaf <altaf98khan66@gmail.com>
2023-03-22 20:21:18 +0530

    Added: Euclidean distance problem using CLI

---
+++ src/com/bridgelabz/functionalprograms/DistanceCLI.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.functionalprograms;
+
+public class DistanceCLI {
+	public static void main(String[] args) {
+
+		int x = Integer.parseInt(args[0]);
+		int y = Integer.parseInt(args[1]);
+		 // Calculating the distance using  Euclidean distance formula
+		double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+		System.out.println("The Euclidean distance from (" + x + ", " + y + ") to the origin (0, 0) is " + distance);
+	}
+
+}
